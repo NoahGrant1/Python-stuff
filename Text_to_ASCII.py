8b954b35aa927f68319b6ec053a1e71b8d7d0a58
Noah Grant <104340106+NoahGrant1@users.noreply.github.com>
2023-07-11 11:38:16 +1000

    Update and rename AsciiCypherGame.py to Text_to_ASCII.py

--- Text_to_ASCII.py
+++ Text_to_ASCII.py
@@ -6,4 +6,5 @@ message = input("Enter Sentence to display as ASCII: ")
 # translate message into ASCII and display it
 for char in message:
   Ascii_values.append(ord(char))
-  print(Ascii_values)
+
+print(Ascii_values)
