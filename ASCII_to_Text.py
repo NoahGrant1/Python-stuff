8d34a64e2f8529fe30f8c5a06ba1c2f8fae97abf
Noah Grant <104340106+NoahGrant1@users.noreply.github.com>
2023-07-11 17:47:33 +1000

    Update ASCII_to_Text.py
    Add Range Checking

--- ASCII_to_Text.py
+++ ASCII_to_Text.py
@@ -1,19 +1,20 @@
-# input ASCII values and display decoded message
 message = []
 exitMenu = False
+# loop through code
 while not exitMenu:
-   print("Enter the ASCII values one at a time and then type decode")
-   selection = input("Enter the ASCII value: ")
+    print("Enter the ASCII values one at a time and then type decode")
+    selection = input("Enter the ASCII value: ")
 
-# Add to message list
-if selection.isnumeric():
-    message.append(int(selection))
-# display the decoded message
- elif selection.lower() == "decode":
-    exitMenu = True
+    # Add to message list
+    # check if numeric and within ASCII value range 
+    if selection.isnumeric() and not int(selection) < 0 and not int(selection) > 255:
+        message.append(int(selection))
+    # display the decoded message
+    elif selection.lower() == "decode":
+        exitMenu = True
+    else:
+        print("Invalid Symbol. Please Enter Again")
 
 # print the message
- for char in message:
+for char in message:
     print(chr(char))
- else:
-    print("Invalid Symbol. Please Enter Again")
