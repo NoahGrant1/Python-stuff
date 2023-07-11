9ddb531c8b3c1a3cedfa03d15658ce416c75d7b6
Noah Grant <104340106+NoahGrant1@users.noreply.github.com>
2023-07-11 11:20:41 +1000

    Create ASCIIUncyhperGame.py
    Basic Cypher Game that converts ASCII characters into a message. Used for education purposes.

---
+++ ASCIIUncyhperGame.py
@@ -0,0 +1,19 @@
+# input ASCII values and display decoded message
+message = []
+exitMenu = False
+while not exitMenu:
+   print("Enter the ASCII values one at a time and then type decode")
+   selection = input("Enter the ASCII value: ")
+
+# Add to message list
+if selection.isnumeric():
+   message.append(int(selection))
+# display the decoded message
+ elif selection.lower() == "decode":
+  exitMenu = True
+
+# print the message
+ for char in message:
+    print(chr(char))
+ else:
+  print("Invalid Symbol. Please Enter Again")
