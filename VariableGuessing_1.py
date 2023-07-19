17aa63bc0f53d71397de3a31f4a1ddcaaa77f87a
Noah Grant <104340106+NoahGrant1@users.noreply.github.com>
2023-07-19 14:52:29 +1000

    Update VariableGuessing_1.py
    Updated Variable Guessing game with additional documentation and new data types. e.g dict and list.

--- VariableGuessing_1.py
+++ VariableGuessing_1.py
@@ -2,15 +2,44 @@
 # author - Noah Grant
 # language - Python
 # Program version 1
+# Programs supports integer, float, 5boolean, dictionary, list, and string.
+# added extra questions is done by adding it to the questions list
+# and adding a new check condition in the answer checking section.
 import random
 
 
+# check if a value is an integer and return bool statement
 def is_int(value):
     try:
         int(value)
         return True
     except ValueError:
         return False
+
+# turn answer into a list
+def make_list(value):
+    # make sure string type and split at space
+    value = str(value)
+    value = value.split(" ")
+    return value
+
+
+# turn answer into a dictionary
+def make_dict(value):
+    # get user input and seperate key and value
+    value = str(value)
+    try:
+        value = value.split(":")
+    except ValueError:
+        return False
+
+    # make the dictionary and assign key and value
+    temp = dict()
+    if len(value) >= 2:
+        temp[value[0]] = value[1]
+        return temp
+
+    return False
 
 
 # game loop
@@ -19,7 +48,9 @@ loop = False
 questions = [("Enter an integer:", "int"),
              ("Enter a boolean: ", "bool"),
              ("Enter a float: ", "float"),
-             ("Enter a string: ", "str")]
+             ("Enter a string: ", "str"),
+             ("Enter a list: (seperate items with space) ", "list"),
+             ("Enter a dictionary: (seperate items with : )", "dict")]
 while not loop:
     # choose random question and get answer from user
     question = random.randint(0, len(questions) - 1)
@@ -39,6 +70,20 @@ while not loop:
     # check boolean question
     elif answer.lower() == ("true" or "false") and questions[question][1] == "bool":
         print("correct")
+    # check list question
+    elif questions[question][1] == "list":
+        # if more then 2 elements and determine respond with answer
+        if len(make_list(answer)) >= 2:
+            print("correct")
+        else:
+            print("incorrect")
+    # check dictionary questions
+    elif questions[question][1] == "dict":
+        # check type supplied from user and compare  - add try and catch errors
+        if type(make_dict(answer)) is dict:
+            print("correct")
+        else:
+            print("incorrect")
     else:
         print("incorrect")
 
