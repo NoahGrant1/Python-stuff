41a02c68946f2ebe59c81dbcba80e33730b398fc
Noah Grant <104340106+NoahGrant1@users.noreply.github.com>
2023-09-05 11:02:21 +1000

    Update SimonGame.py
    Expanded GUI to make a proper Game
    
    Issues:
    Check answer button does not work correctly in some use cases.

--- SimonGame.py
+++ SimonGame.py
@@ -1,23 +1,35 @@
 # Simon Game
 # Author: Noah Grant
 # issues:
-#   implement timed input?
-#   clear user input box after checking answer
-
-#  instructions:
-#  use the GUI
+#  rework check function to accept nothing
 
 from tkinter import *
+import time
 import random
 
-
-# global variables
 class game_data:
     count = 1
     colours = ["red", "blue", "yellow", "green"]
     question = []
     max_count = 10
     correct_answer = ""
+    user_answer = ""
+
+# global variables
+def clear_data():
+    game_data.correct_answer = ""
+    game_data.user_answer = ""
+
+# reset the game difficulty to 1
+def reset_diff():
+    game_data.count = 1
+    message = display.create_text(50, 100, text="reset difficulty")
+    display.moveto(message, 50, 50)
+    display.update()
+
+    # remove message from screen
+    display.after(2000, display.delete(message))
+    display.update()
 
 
 # generate a random combination of colours and expected answer
@@ -39,29 +51,6 @@ def generate_question():
     display_colour(game_data.question)
 
 
-# display incorrect or correct based on user input
-def check_answer_function(correct_answer: str):
-    # correct answer
-    if user_input.get(1.0, "end-1c") == correct_answer:
-        message = display.create_text(50, 100, text="correct, increasing difficulty")
-        # increase the difficulty via count if correct answer
-        if game_data.count != game_data.max_count:
-            game_data.count += 1
-    # incorrect answer
-    else:
-        message = display.create_text(50, 50, text="Incorrect")
-
-    # display message in game
-    #game_data.correct_answer = ""
-    user_input.delete(1.0, 'end')
-    display.moveto(message, 50, 50)
-    display.update()
-
-    # remove message from screen
-    display.after(2000, display.delete(message))
-    display.update()
-
-
 # Display each colour for 1 secs
 def display_colour(question: list):
     # loop through colours and display for 1 sec
@@ -71,51 +60,90 @@ def display_colour(question: list):
         display.moveto(shape, 50, 50)
         display.update()
 
-        # remove colour
+        # remove colour and delay next colour
         display.after(1000, display.delete(shape))
         display.update()
+        time.sleep(0.5)
+
+
+# use 4 buttons for input
+def check_answer_function(colour: str):
+    # If all colours selected then proceed
+    if len(game_data.user_answer) == game_data.count:
+        # correct answer
+        if game_data.user_answer == game_data.correct_answer:
+            message = display.create_text(50, 100, text="correct, increasing difficulty")
+            clear_data()
+            # increase the difficulty via count if correct answer
+            if game_data.count != game_data.max_count and colour != "":
+                game_data.count += 1
+        # incorrect answer
+        else:
+            message = display.create_text(50, 50, text="Incorrect")
+            clear_data()
+
+        # display message in game
+        display.moveto(message, 50, 50)
+        display.update()
+
+        # remove message from screen
+        display.after(2000, display.delete(message))
+        display.update()
+
+    # Add new character to existing answer
+    else:
+        game_data.user_answer += colour
 
 
 # GUI setup commands
 root = Tk()
-root.geometry("600x600")
+root.geometry("500x500")
 root.title(" Simon Game ")
+root.configure(bg='gray')
 display = Canvas(root, width=200, height=150)
+display.configure(bg='gray', highlightbackground='black')
 
 # game description
 intro = Label(text="Welcome to the Simon Game \n"
-                   "Enter first letter of each colour with no spaces e.g rgby, rrrr \n"
-                   "Once Game is ended press exit to close the program. ")
+                   "Click start and enter the sequence of colours used the buttons \n"
+                   "Press check Button when done to reveal result",
+              bg="gray")
 
 # start the next round button
-start_game = Button(root, height=2,
-                    width=20,
-                    text="Start",
-                    command=lambda: generate_question())
-
-# user input box
-user_input = Text(root,
-                  height=5,
-                  width=20)
-
-# check answer button and increase difficulty
-check_answer_button = Button(root, height=2,
-                             width=20,
-                             text="Check Answer",
-                             command=lambda: check_answer_function(game_data.correct_answer))
+start_game = Button(root, height=2, width=20, text="Start",
+                    command=lambda: generate_question(), bg='slategray4')
 
 # end the game button
-end_game = Button(root, height=2,
-                  width=20,
-                  text="End",
-                  command=lambda: exit(0))
+end_game = Button(root, height=2, width=20, text="End",
+                  command=lambda: exit(0), bg='slategray4')
 
+# check the user inputted answer
+check_answer_button = Button(root, height=2, width=20, text="Check"
+                             , command=lambda: check_answer_function(""), bg='slategray4')
 
-intro.pack()
-start_game.pack()
-user_input.pack()
-display.pack()
-check_answer_button.pack()
-end_game.pack()
+# reset difficulty  button
+reset_difficulty = Button(root, height=2, width=20, text="Reset Difficulty"
+                          , command=lambda: reset_diff(), bg='slategray4')
+
+# buttons for user input
+red_button = Button(root, bg=game_data.colours[0], width=20,
+                    command=lambda: check_answer_function("r"))
+blue_button = Button(root, bg=game_data.colours[1], width=20,
+                     command=lambda: check_answer_function("b"))
+yellow_button = Button(root, bg=game_data.colours[2], width=20,
+                       command=lambda: check_answer_function("y"))
+green_button = Button(root, bg=game_data.colours[3], width=20,
+                      command=lambda: check_answer_function("g"))
+
+# GUI design
+intro.grid(row=0, column=1, sticky=W, pady=2, columnspan=3)
+start_game.grid(row=1, column=1, sticky=W, pady=2, columnspan=3)
+display.grid(row=2, column=1, sticky=W, pady=2, columnspan=3)
+red_button.grid(row=4, column=1, sticky=W, pady=2, columnspan=3)
+blue_button.grid(row=6, column=1, sticky=W, pady=2, columnspan=3)
+check_answer_button.grid(row=5, column=1, sticky=W, pady=2, columnspan=1)  # centre of controls
+yellow_button.grid(row=5, column=0, sticky=W, pady=2, columnspan=1)
+green_button.grid(row=5, column=2, sticky=W, pady=2, columnspan=1)
+reset_difficulty.grid(row=7, column=1, sticky=W, pady=2, columnspan=3)
+end_game.grid(row=8, column=1, sticky=W, pady=2, columnspan=3)
 mainloop()
-
